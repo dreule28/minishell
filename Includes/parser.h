9991bd8c6e7ae2ee3170fc6da873dfd6e0714cd4
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-03-11 10:52:33 +0100

    FEAT(parser.h): added rough data structure using doubly linked list and meta struct. Implemented enum's

--- Includes/parser.h
+++ Includes/parser.h
@@ -6,6 +6,36 @@
 # include <stdio.h>
 //Includes -- END
 
+//Structs -- BEGIN
+typedef enum type
+{
+	WORD,
+	BUILTIN,
+	S_QUOTES,
+	D_QUOTES,
+	INFILE,
+	OUTFILE,
+	HEREDOC,
+	APPEND,
+	PIPE
+}			t_type;
+
+typedef struct s_token
+{
+	t_type			token;
+	struct s_token	*next;
+	struct s_token	*prev;
+}					t_token;
+
+typedef struct s_token_list
+{
+	t_token	*head;
+	t_token	*tail;
+	int		size;
+	char	*input;
+}					t_token_list;
+//Structs -- END
+
 //Functions -- BEGIN
 void	from_parser();
 //Functions -- END
