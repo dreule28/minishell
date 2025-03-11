9a1543f0815665a26a6a7cfe0602cb53308111a1
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-03-11 12:04:13 +0100

    BUILT(minishell.h): added project strucutre in minishell header.

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -8,6 +8,52 @@
 # include "executor.h"
 //Includes -- END
 
+
+//Structs -- BEGIN
+typedef struct s_file_node
+{
+	char				*filename;
+	int					redir_type;
+	struct s_file_node	*next;
+}	t_file_node;
+
+typedef struct s_file_list
+{
+	t_file_node	*head;
+	t_file_node	*tail;
+	ssize_t		size;
+}	t_file_list;
+
+typedef struct s_cmd_node
+{
+	struct s_cmd_node	*next;
+	int					cmd_type;
+	char				**cmd;
+	t_file_list			*files;
+}	t_cmd_node;
+
+typedef struct s_cmd_list
+{
+	t_cmd_node	*head;
+	t_cmd_node	*tail;
+	ssize_t		size;
+}	t_cmd_list;
+
+typedef enum CMD_TYPE
+{
+	BUILTIN,
+	CMD,
+	PIPE
+}	CMD_TYPE;
+
+typedef enum REDIR_TYPE
+{
+	INFILE,
+	OUTFILE,
+	HEREDOC,
+	OUTFILE_APPEND
+}	REDIR_TYPE;
+//Structs -- END
 //Functions -- BEGIN
 
 //Functions -- END
