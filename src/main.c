6fcd7e1630701f7226581974e195f7d8f7106b6d
gzovkic <g.zovkic@web.de>
2025-03-11 12:56:56 +0100

    test git global settings

--- src/main.c
+++ src/main.c
@@ -5,5 +5,7 @@ int	main(int argc, char **argv, char **env)
 	from_executor();
 	from_parser();
 	printf("This is a test from main\n");
+	printf("This is a test from main\n");
+	printf("This is a test from main\n");
 	return (0);
 }
