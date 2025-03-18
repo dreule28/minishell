510d273a077c4b460f24b45cf4bb1cf20a3eddb2
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-03-18 15:32:26 +0100

    CHORE(lexer.c & check_actions.c): moved functions for checking into seperate file

--- src/parser/lexer/lexer.c
+++ src/parser/lexer/lexer.c
@@ -1,50 +1,20 @@
 #include "minishell.h"
 
-void	check_quotes(t_token_list *list, char *input, int i)
+void	handle_word_or_arg(t_token_list *list, char *input, int *i)
 {
 	int		start;
-	char	quote;
+	char	*token_value;
 
-	quote = input[i++];
-	start = i;
-	while (input[i] && input[i] != quote)
-		i++;
-	if (!input[i])
+	start = *i;
+	while (input[*i] && !(input[*i] == ' ' || input[*i] == '\t')
+			&& !is_special_char(&input[*i])
+			&& input[*i] != '"' && input[*i] != '\'')
+		(*i)++;
+	if (start != *i)
 	{
-		printf("Syntax error: Open quotes!\n");
-		return ;
+		token_value = ft_substr(input, start, *i - start);
+		add_token(list, token_value , TK_WORD);
 	}
-	add_token(list, ft_substr(input, start, i - start), D_QUOTES);
-	i++;
-}
-
-void	check_redirs(t_token_list *list, char *input, int i)
-{
-	int	redir_type;
-	int	start;
-
-	redir_type = get_redir_type(input, i);
-	while (is_space(input[i]))
-		i++;
-	start = i;
-	while (input[i] && !is_space(input[i]) && !is_special_char(input[i]))
-		i++;
-	if (start == i)
-	{
-		printf("Syntax error: Missing filename!");
-		return ;
-	}
-	add_token(list, ft_substr(input, start, i - start), redir_type);
-}
-
-void	handle_word_or_arg(t_token_list *list, char *input, int i)
-{
-	int	start;
-
-	start = i;
-	while (input[i] && !is_space(input[i]) && !is_special_char(input[i]))
-		i++;
-	add_token(list, ft_substr(input, start, i - start), TK_WORD);
 }
 
 t_token_list	*lexer(char *input)
