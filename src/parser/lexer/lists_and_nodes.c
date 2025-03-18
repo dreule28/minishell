c5b521c5eef8ea711fe8485316392082549ff9f9
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-03-18 16:34:24 +0100

    FIX(lists_and_nodes.c): correct memory allocation for token list and improved token addition with null checks

--- src/parser/lexer/lists_and_nodes.c
+++ src/parser/lexer/lists_and_nodes.c
@@ -5,7 +5,7 @@ t_token_list	*init_token_list()
 {
 	t_token_list	*list;
 
-	list = ft_malloc(sizeof(t_cmd_list), 1);
+	list = ft_malloc(sizeof(t_token_list), 1);
 	if (!list)
 		return (NULL);
 	list->head = NULL;
@@ -19,12 +19,19 @@ void	add_token(t_token_list *list, char *token, int type)
 {
 	t_token	*new_token;
 
+	if (!token || !*token)
+		return;
 	new_token = ft_malloc(sizeof(t_token), 1);
 	if (!new_token)
 		return ;
 	new_token->next = NULL;
 	new_token->token = type;
-	new_token->value = token;
+	new_token->value = ft_strdup(token);
+	if (!new_token->value)
+	{
+		free(new_token);
+		return ;
+	}
 	if (!list->head)
 		list->head = new_token;
 	else
