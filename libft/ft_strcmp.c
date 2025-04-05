4568d538ffb310e85700cc4207098ff27722bb10
Daniel Reule <dreule@2-C-9.42heilbronn.de>
2025-04-05 04:05:38 +0200

    NORM(ft_strcmp): LOL

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 04:01:30 by dreule            #+#    #+#             */
-/*   Updated: 2025/04/05 04:01:38 by dreule           ###   ########.fr       */
+/*   Updated: 2025/04/05 04:05:14 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
 	while ((s1[i] && s2[j]) && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
