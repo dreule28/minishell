d6c20918d24d72b32254b2f03c5092b70721bcb2
dreule <danielreule28@gmail.com>
2025-04-23 16:38:50 +0200

    FIX(execute.c): added NULL check incase cmd_list fails

--- src/executor/execute.c
+++ src/executor/execute.c
@@ -1,14 +1,13 @@
-#include "executor.h"
 #include "minishell.h"
 
 void execute(t_env_list *env_list, t_cmd_list *cmd_list)
 {
-	// DEBUG_INFO("starting execution\n");
-
-	if(cmd_list->size == 1 && cmd_list->head->cmd_type == BUILTIN)
+	if (!cmd_list)
+		return ;
+	if (cmd_list->size == 1 && cmd_list->head->cmd_type == BUILTIN)
 		single_builtin_execution(cmd_list->head, env_list);
 	else if (!cmd_list->syntax_error)
-		execution(cmd_list, env_list); // normal execution
+		execution(cmd_list, env_list);
 	return ;
 }
 
@@ -46,7 +45,8 @@ void execution_loop(t_cmd_list *cmd_list, t_env_list *env_list)
 	while(cmd_node)
 	{
 		if(create_here_doc_files(cmd_node, env_list) == -1)
-			ft_putstr_fd("error in heredoc creation\n", 2);
+			return ;
+		// ft_putstr_fd("arcg\n" , 2);
 		if(cmd_node->next)
 			pipe_creation(pipe_fd);
 		pid =  fork();
@@ -61,7 +61,7 @@ void execution_loop(t_cmd_list *cmd_list, t_env_list *env_list)
 			else
 				child_proccess(cmd_node, env_list);
 			clean_up();
-			exit(0);
+			exit(1);
 		}
 		set_pipes_parent(pipe_fd, prev_pipe_fd);
 		cmd_node = cmd_node->next;
