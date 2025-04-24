25cece96607dd232aad9c9beab55a7e5439dd3a4
dreule <danielreule28@gmail.com>
2025-04-24 10:38:10 +0200

    CHORE(main.c): first small cleanup. work in progress.

--- src/main.c
+++ src/main.c
@@ -1,59 +1,28 @@
 #include "minishell.h"
 
-int	get_exit_status(int new_code, int set)
+int	*exit_code(void)
 {
-	static int	status = 0;
+	static int	exit_code;
 
-	if (set)
-		status = new_code;
-	return (status);
-}
-
-// Helper function to print environment list contents
-void print_env_list(t_env_list *list)
-{
-    t_env_node *current;
-    int i = 0;
-
-    if (!list)
-    {
-        DEBUG_ERROR("Cannot print NULL env list");
-        return;
-    }
-
-    printf("\n%s=== Environment List ===%s\n", COLOR_GREEN, COLOR_RESET);
-    printf("Total environment variables: %zu\n", list->size);
-
-    current = list->head;
-    while (current)
-    {
-        printf("ENV %d: [%s%s%s=%s%s%s]\n",
-               i++, COLOR_CYAN, current->type, COLOR_RESET,
-               COLOR_YELLOW, current->value, COLOR_RESET);
-        current = current->next;
-    }
-    printf("%s=====================%s\n\n", COLOR_GREEN, COLOR_RESET);
+	return (&exit_code);
 }
 
 int main(int argc, char **argv, char **env)
 {
-    char *prompt;
-    t_token_list *tk_list;
-    t_cmd_list *cmd_list;
-    t_env_list *env_list;
-    (void)argc;
-    (void)argv;
+	char			*prompt;
+	t_token_list	*tk_list;
+	t_cmd_list		*cmd_list;
+	t_env_list		*env_list;
+	(void)argc;
+	(void)argv;
 
 
-    gc_init();
-    env_list = get_envs(env);
+	gc_init();
+	env_list = get_envs(env);
 	start_signals();
-    // DEBUG_INFO("Environment list initialized");
-    // print_env_list(env_list);
-
-    while (1)
-    {
-        prompt = readline("2, 3 Years dagestan forget > ");
+	while (1)
+	{
+		prompt = readline("2, 3 Years dagestan forget > ");
 		if (!prompt)
 		{
 			ft_putstr_fd("exit\n", 2);
@@ -62,42 +31,35 @@ int main(int argc, char **argv, char **env)
 		}
 		if (*prompt == '\0')
 			continue;
-			// break;
-        if (prompt)
-        {
-            gc_add(prompt);
-            // DEBUG_INFO("Processing input: [%s]", prompt);
+		if (prompt)
+		{
+			gc_add(prompt);
 
 			if (ft_strlen(prompt) > 0)
 				add_history(prompt);
-            tk_list = lexer(prompt);
-            DEBUG_INFO("Lexer completed");
-            print_tokens(tk_list);
+			tk_list = lexer(prompt);
+			if (!tk_list)
+				continue;
 
-            tk_list = process_token_list(tk_list);
-            DEBUG_INFO("Token processing completed");
-            print_tokens(tk_list);
+			tk_list = process_token_list(tk_list);
+			if (!tk_list)
+				continue;
 
 			segment_tokens(tk_list, env_list);
-			DEBUG_INFO("Variable expansion complete");
-            cmd_list = token_to_cmd(tk_list);
-			// DEBUG_INFO("REDIRTYPE: %d\n",cmd_list->tail->files->head->redir_type);
-            // DEBUG_INFO("Command list generation completed");
-            print_cmd_list(cmd_list);
+			cmd_list = token_to_cmd(tk_list);
+			if (!cmd_list)
+				continue;
 
-            if (ft_strcmp(prompt, "exit") == 0)
-                break;
-
-            // DEBUG_INFO("Executing command");
-			DEBUG_INFO("env list : %s\n", env_list->head->type);
-
-            execute(env_list, cmd_list);
-            delete_tmp_files("tmp");
-			// check_fds();
-        }
-    }
-    clean_up();
-    return (0);
-}
+			if (!execute(env_list, cmd_list))
+			{
+				delete_tmp_files("tmp");
+				break ;
+			}
+			delete_tmp_files("tmp");
+		}
+	}
+	clean_up();
+	return (0);
+	}
 
 // dorker valgrind --leak-check=full  --show-leak-kinds=all --suppressions=sub.sub --track-fds=yes ./minishell
