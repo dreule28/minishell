d7507d1bd196679c4a950629d0e6694821d1d5bb
dreule <danielreule28@gmail.com>
2025-05-01 16:04:30 +0200

    NORM

--- src/parser/lexer/lexer_utils2.c
+++ src/parser/lexer/lexer_utils2.c
@@ -2,11 +2,11 @@
 
 bool	is_empty_quotes(char *input, int i)
 {
-	if (input[i] == '"' && input[i + 1] == '"' &&
-		(input[i+2] == ' ' || input[i+2] == '\t' || !input[i+2]))
+	if (input[i] == '"' && input[i + 1] == '"'
+		&& (input[i + 2] == ' ' || input[i + 2] == '\t' || !input[i + 2]))
 		return (true);
-	if (input[i] == '\'' && input[i + 1] == '\'' &&
-		(input[i+2] == ' ' || input[i+2] == '\t' || !input[i+2]))
+	if (input[i] == '\'' && input[i + 1] == '\''
+		&& (input[i + 2] == ' ' || input[i + 2] == '\t' || !input[i + 2]))
 		return (true);
 	return (false);
 }
