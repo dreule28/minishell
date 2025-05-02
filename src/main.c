09ef364b06983ab41300fdb52ba9757914af7268
dreule <danielreule28@gmail.com>
2025-05-02 13:01:36 +0200

    CHORE(main.c); split main into helpers

--- src/main.c
+++ src/main.c
@@ -1,99 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 11:53:51 by dreule            #+#    #+#             */
+/*   Updated: 2025/05/02 13:00:38 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int	*exit_code(void)
+t_env_list	*setup_env_list(void)
 {
-	static int	exit_code;
-
-	return (&exit_code);
-}
-
-t_env_list 	*setup_env_list()
-{
-	t_env_list *env_list;
-	char *pwd;
+	t_env_list	*env_list;
+	char		*pwd;
 
 	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		ft_putstr_fd("minishell: getcwd error\n", 2);
+		return (NULL);
+	}
 	gc_add(pwd);
 	env_list = init_env_list();
+	if (!env_list)
+		return (NULL);
 	add_env_node(env_list, "SHLVL", "2");
 	add_env_node(env_list, "PWD", pwd);
 	add_env_node(env_list, "OLDPWD", gc_strdup(""));
-	return(env_list);
+	return (env_list);
 }
 
-int main(int argc, char **argv, char **env)
+t_env_list	*initialize_shell(char **env)
 {
-	char			*prompt;
-	t_token_list	*tk_list;
-	t_cmd_list		*cmd_list;
-	t_env_list		*env_list;
-	(void)argc;
-	(void)argv;
-
+	t_env_list	*env_list;
 
 	gc_init();
-	if(env[0] == NULL)
+	if (!env || !env[0])
 		env_list = setup_env_list();
 	else
 		env_list = get_envs(env);
+	if (!env_list)
+	{
+		ft_putstr_fd("minishell: failed to initialize environment\n", 2);
+		*exit_code() = 1;
+		return (NULL);
+	}
 	start_signals();
+	return (env_list);
+}
+
+char	*get_prompt(void)
+{
+	char	*prompt;
+	char	*line;
+
+	if (isatty(STDIN_FILENO))
+	{
+		prompt = readline("minishell$ ");
+		if (prompt && *prompt)
+			add_history(prompt);
+		return (prompt);
+	}
+	line = get_next_line(STDIN_FILENO);
+	if (!line)
+		return (NULL);
+	prompt = ft_strtrim(line, "\n");
+	free(line);
+	return (prompt);
+}
+
+int	process_command(char *prompt, t_env_list *env_list)
+{
+	t_token_list	*tk_list;
+	t_cmd_list		*cmd_list;
+
+	if (!prompt)
+		return (0);
+	if (*prompt == '\0')
+		return (*exit_code() = 0, 1);
+	gc_add(prompt);
+	tk_list = lexer(prompt);
+	if (!tk_list)
+		return (1);
+	tk_list = process_token_list(tk_list);
+	if (!tk_list)
+		return (1);
+	segment_tokens(tk_list, env_list);
+	cmd_list = token_to_cmd(tk_list);
+	if (!cmd_list)
+		return (1);
+	if (!execute(env_list, cmd_list))
+	{
+		delete_tmp_files("tmp");
+		return (0);
+	}
+	delete_tmp_files("tmp");
+	return (1);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_env_list	*env_list;
+	char		*prompt;
+	int			keep_running;
+
+	(void)argc;
+	(void)argv;
+	env_list = initialize_shell(env);
+	if (!env_list)
+		return (clean_up(), 1);
 	while (1)
 	{
-		if (isatty(fileno(stdin)))
-		{
-			prompt = readline("2, 3 Years dagestan forget > ");
-			if (prompt && ft_strlen(prompt) > 0)
-            	add_history(prompt);
-		}
-		else
-		{
-			char *line;
-			line = get_next_line(fileno(stdin));
-			if(!line)
-				break ;
-			prompt = ft_strtrim(line, "\n");
-			free(line);
-		}
+		prompt = get_prompt();
 		if (!prompt)
 		{
-			ft_putstr_fd("exit\n", 2);
-			clean_up();
-			exit(*exit_code());
+			if (isatty(STDIN_FILENO))
+				ft_putstr_fd("exit", 2);
+			break ;
 		}
-		if (*prompt == '\0')
-		{
-			*exit_code() = 0;
-			continue;
-		}
-		if (prompt)
-		{
-			gc_add(prompt);
-
-			// if (ft_strlen(prompt) > 0)
-			// 	add_history(prompt);
-			tk_list = lexer(prompt);
-			if (!tk_list)
-				continue;
-			tk_list = process_token_list(tk_list);
-			if (!tk_list)
-				continue;
-
-			segment_tokens(tk_list, env_list);
-			cmd_list = token_to_cmd(tk_list);
-			if (!cmd_list)
-				continue;
-
-			if(execute(env_list, cmd_list) == 0)
-			{
-				delete_tmp_files("tmp");
-				break;
-			}
-			delete_tmp_files("tmp");
-		}
+		keep_running = process_command(prompt, env_list);
+		if (!keep_running)
+			break ;
 	}
 	clean_up();
-	exit(*exit_code());
-	return (0);
-	}
-
-// dorker valgrind --leak-check=full  --show-leak-kinds=all --suppressions=sub.sub --track-fds=yes ./minishell
+	return (*exit_code());
+}
