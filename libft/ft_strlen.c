1e0263bd498300d65e3e4f638da428f61491ab41
dreule <danielreule28@gmail.com>
2025-05-06 14:52:17 +0200

    fix(strlen): add null check for input string to prevent potential crashes

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:45:48 by dreule            #+#    #+#             */
-/*   Updated: 2024/10/09 10:57:24 by dreule           ###   ########.fr       */
+/*   Updated: 2025/05/06 12:18:09 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *str)
 	size_t	len;
 
 	len = 0;
+	if (str == NULL)
+		return ((size_t) NULL);
 	while (*str != '\0')
 	{
 		str++;
